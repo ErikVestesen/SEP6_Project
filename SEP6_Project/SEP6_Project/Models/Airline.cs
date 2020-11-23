51c266fba95209b6c4d9e38ab64d6208f10fd190
ArshadIT <37140618+ArshadIT@users.noreply.github.com>
2020-11-23 21:37:27 +0100

    classes united airline

---
+++ SEP6_Project/SEP6_Project/Models/Airline.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Plane
+{
+    [Key]
+    public string Tailnum { get; set; }
+    public int? Year { get; set; }
+    public string Type { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public int Engines { get; set; }
+    public int Seats { get; set; }
+    public int? Speed { get; set; }
+    public string? Engine { get; set; }
+}
---
+++ SEP6_Project/SEP6_Project/Models/Airport.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Airport
+{
+
+    public string Faa { get; set; }
+    public string Name { get; set; }
+    public double Lat { get; set; }
+    public double Lon { get; set; }
+    public int Alt { get; set; }
+    public int Tz { get; set; }
+    public string Dst { get; set; }
+    public string Tzone { get; set; }
+}

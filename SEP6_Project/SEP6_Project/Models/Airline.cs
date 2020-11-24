0c1d2d6ccb95a40046853a7c370c67317362703b
ArshadIT <37140618+ArshadIT@users.noreply.github.com>
2020-11-24 08:57:05 +0100

    airline class update

--- SEP6_Project/SEP6_Project/Models/Airline.cs
+++ SEP6_Project/SEP6_Project/Models/Airline.cs
@@ -1,15 +1,16 @@
 using System;
 
-public class Plane
+public class Airline
 {
-    [Key]
-    public string Tailnum { get; set; }
-    public int? Year { get; set; }
-    public string Type { get; set; }
-    public string Manufacturer { get; set; }
-    public string Model { get; set; }
-    public int Engines { get; set; }
-    public int Seats { get; set; }
-    public int? Speed { get; set; }
-    public string? Engine { get; set; }
+
+   public string Name 
+        {
+    get;
+    set;
+    }
+    public string Carrier
+    {
+    get;
+    set;
+    }
 }
--- SEP6_Project/SEP6_Project/Models/Flight.cs
+++ SEP6_Project/SEP6_Project/Models/Flight.cs
@@ -1,4 +1,4 @@
-using System;
+Yearusing System;
 
 public class Flight
 {

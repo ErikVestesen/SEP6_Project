62217f45476467b9797008057a34d66384d51d3b
CasperJuhl <casperbjuhl@gmail.com>
2020-12-08 13:37:49 +0100

    Setup for new page

---
+++ SEP6_Project/SEP6_Project/Controllers/TempController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP6_Project.Controllers
+{
+    public class TempController : Controller
+    {
+        public IActionResult TempIndex()
+        {
+            return View();
+        }
+    }
+}

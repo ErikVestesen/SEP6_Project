c94a53474c111edf661ef8635fb7c8ae67691ddc
CasperJuhl <casperbjuhl@gmail.com>
2020-12-10 15:13:00 +0100

    Last change

---
+++ SEP6_Project/SEP6_Project/Controllers/DepManController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP6_Project.Controllers
+{
+    public class DepManController : Controller
+    {
+        public IActionResult Depindex()
+        {
+            return View();
+        }
+    }
+}

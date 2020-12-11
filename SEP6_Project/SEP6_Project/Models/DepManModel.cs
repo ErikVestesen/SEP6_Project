7dbc49885ab726dacd6bea956fd6f9218d8b725f
Erik Vestesen <erik.vestesen@hotmail.com>
2020-12-11 12:03:44 +0100

    mean dep & arr manufacturer airbus models

---
+++ SEP6_Project/SEP6_Project/Models/DepManModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP6_Project.Models
+{
+    public class DepManModel
+    { 
+        // Mean departure delays
+        public double mean_dep_LGA { get; set; }
+        public double mean_dep_EWR { get; set; }
+        public double mean_dep_JFK { get; set; }
+
+        // Mean arrival delays
+        public double mean_arr_LGA { get; set; }
+        public double mean_arr_EWR { get; set; }
+        public double mean_arr_JFK { get; set; }
+
+
+        // Manufacturer
+        public List<Manufacturer> big_manufacturers { get; set; }
+
+        public List<Manufacturer> manufacturers_with_flights { get; set; }
+
+        public List<(string, int)> airbus_models { get; set; }
+
+
+    }
+}
---
+++ SEP6_Project/SEP6_Project/Models/Manufacturer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP6_Project.Models
+{
+    public class Manufacturer
+    {
+        public string name { get; set; }
+        public int plane_count { get; set; }
+        public int flight_count { get; set; }
+
+        public Manufacturer() { }
+    }
+}
